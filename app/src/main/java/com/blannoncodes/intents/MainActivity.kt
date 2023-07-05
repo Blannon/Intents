ec9ee4f0df1451169a4777cb8ce1dc8098d099d1
Blannon <63664876+Blannon@users.noreply.github.com>
2023-07-05 05:29:32 +0300

    Final Commit

--- app/src/main/java/com/blannoncodes/intents/MainActivity.kt
+++ app/src/main/java/com/blannoncodes/intents/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.blannoncodes.intents
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Explicit Intent
+        val explicitButton = findViewById<Button>(R.id.ExplicitButton)
+
+        explicitButton.setOnClickListener {
+            val explicitIntent =Intent(this,SecondActivity::class.java)
+            startActivity(explicitIntent)
+            finish()
+        }
+
+        //Implicit Intent
+        val url = "https://www.google.com"
+        val implicitButton= findViewById<Button>(R.id.ImplicitButton)
+        implicitButton.setOnClickListener {
+            val implicitIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(implicitIntent)
+        }
     }
 }
